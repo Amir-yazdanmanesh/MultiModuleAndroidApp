6041d6b87ffe2e7f4c3a21bcca7dd2179676da0e
am.yazdanmanesh <amir.yazdanmanesh@gmail.com>
2022-12-14 00:46:35 +0330

    add cache to HeroInteractions factory

--- hero/hero-interactors/src/main/java/com/yazdanmanesh/hero_interactors/HeroInteractors.kt
+++ hero/hero-interactors/src/main/java/com/yazdanmanesh/hero_interactors/HeroInteractors.kt
@@ -1,16 +1,20 @@
 package com.yazdanmanesh.hero_interactors
 
+import com.squareup.sqldelight.db.SqlDriver
+import com.yazdanmanesh.hero_datasource.cache.HeroCache
 import com.yazdanmanesh.hero_datasource.network.HeroService
 
 class HeroInteractions(
     val getHeroes: GetHeroes
 ) {
-    companion object Factory{
-        fun build():HeroInteractions{
+    companion object Factory {
+        fun build(sqlDriver: SqlDriver): HeroInteractions {
             val service = HeroService.build()
+            val cache = HeroCache.build(sqlDriver)
             return HeroInteractions(
                 getHeroes = GetHeroes(
-                    service = service
+                    service = service,
+                    cache = cache
                 )
             )
         }
